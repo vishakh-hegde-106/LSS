f143cb69b17705a2b5d09bc1e79ca90f86e630eb
VJ_Hegde <99704202+vishakhhegde-code@users.noreply.github.com>
2023-03-14 18:28:54 +0530

    Add files via upload
    simple  arithmatic operations program of shell scripting

---
+++ arithmaticop.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+#purpose:for arithmatic operations
+#created date:mar 14 2023
+#START
+
+echo -e "enter a value \c"
+read -r a
+echo -e "enter second value \c"
+read -r b
+
+echo "a+b value is $((a+b))"
+echo "a-b value is $((a-b))"
+echo "a*b value is $((a*b))"
+echo "a/b value is $((a/b))"
+echo "a%b value is $((a%b))"
+
+
+#END
