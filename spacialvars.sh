4eb499493b69e5fc5bbfdb0803408f31f1b9dc0a
VJ_Hegde <99704202+vishakhhegde-code@users.noreply.github.com>
2023-03-18 19:18:51 +0530

    Add files via upload

---
+++ spacialvars.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Purpose: To learn special variables
+#Version:1.0
+#Created Date:Mar 18 2023
+#Modified Date:
+# START #
+echo "'$*' output is $*"
+echo "'$#' output is $#"
+echo "'$1 & $2' output $1 and $2"
+echo "'$@' output of $@"
+echo "'$?' output is $?"
+echo "'$$' output is $$"
+#sleep 400 &
+echo "'$!' output is $!"
+
+echo "'$0' your current program name is $0"
+
+# END #
