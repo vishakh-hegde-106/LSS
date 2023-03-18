38376e7f1f4f383b50b921b2a7d1734d0e18828b
VJ_Hegde <99704202+vishakhhegde-code@users.noreply.github.com>
2023-03-18 18:40:53 +0530

    Add files via upload

---
+++ addpractice.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+##################################################
+# Purpose: practice simple addition of 2 numbers in shells script
+# Version:1.0
+# Created Date:Mar 18 2023
+##################################################
+# START #
+
+echo "addition of X+Y"
+echo  "Enter X"
+read -r X
+echo  "Enter Y"
+read -r Y
+echo  "X+Y = $((X + Y)) "
+
+# END #
