2666636def4e2e38e8452a4e3e2d50d9ca63d1bb
vishakh <vishakhajhegde.mca22@rvce.edu.in>
2023-03-20 22:20:41 +0530

    hiiii

---
+++ ifstmt.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Purpose: If statement example
+#Version:1.0
+#Modified Date:
+# START #
+echo -e "Please provide Value below ten: \c"
+read -r value
+
+if [ $value -le 10 ]
+then
+echo "You provided value is $value"
+touch /tmp/test{1..100}.txt
+echo "Script completed successfully"
+fi
+
+# END #
